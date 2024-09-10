23d59746bc54f21122ad936fa15d65f1fe991af6
omxchavan <chavanom775@gmail.com>
2024-09-10 23:20:20 +0530

    missing number

---
+++ day_2.cpp
@@ -0,0 +1,17 @@
+// fond the same problem on leet code hence solved it there and same is the solution
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int a=nums.size();
+        sort(nums.begin(),nums.end());
+        for(int i=1;i<nums.size();i++){
+            if (nums[i]!=i){
+                a=i;
+            }
+            
+        }
+       return a;
+    }
+     
+};
