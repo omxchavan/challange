125dc35e735235eb4c66f6d16e18ceda0a755c67
omxchavan <chavanom775@gmail.com>
2024-09-11 19:11:49 +0530

    day_3

--- day_3.cpp
+++ day_3.cpp
@@ -1,16 +1,16 @@
 // found the same problem on leet code hence solved it there and same is the solution
+
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int a=nums.size();
         sort(nums.begin(),nums.end());
         for(int i=0;i<nums.size();i++){
             if (nums[i]!=i){
-                a=i;
+                return i;
             }
             
         }
-       return a;
+       return nums.size();
     }
      
 };
